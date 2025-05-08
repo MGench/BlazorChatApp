c22ace3ebd0692229e8ce89481c5356d7c682365
mustafa.genc@hotmail.co.uk <mustafa.genc@hotmail.co.uk>
2025-05-08 16:51:41 +0100

    Created chatdb context

---
+++ Models/ChatDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorChatApp.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ChatDbContext : DbContext
+    {
+        public DbSet<ChatMessage> Messages { get; set; }
+
+        public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options) { }
+    }
+
+}
