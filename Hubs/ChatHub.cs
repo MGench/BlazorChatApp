2eb4340e562100a9e6623445a7cd580f50f82b54
mustafa.genc@hotmail.co.uk <mustafa.genc@hotmail.co.uk>
2025-05-08 16:46:16 +0100

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorChatApp.Hubs
+
+{
+    using Microsoft.AspNetCore.SignalR;
+    using System.Threading.Tasks;
+
+    public class ChatHub : Hub
+    {
+        // This method will be called when a user sends a message.
+        public async Task SendMessage(string user, string message)
+        {
+            // Broadcast the message to all connected clients.
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+
+}
---
+++ Models/ChatMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorChatApp.Models
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; } // Primary key for storing each chat message.
+        public string User { get; set; } // Stores the sender's name.
+        public string Message { get; set; } // Stores the chat message text.
+        public DateTime Timestamp { get; set; } // Stores the time the message was sent.
+    }
+
+}
